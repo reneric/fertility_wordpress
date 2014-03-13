412e6ff9513f0f17a44307bbf64f4e5d1e426376
Ren Simmons <rsimmo4@gmail.com>
2014-03-13 17:37:26 -0500

    css

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -55,13 +55,9 @@
 	  <tr>
 	    <td width="300" height="62" align="right" valign="center" class="searchit">
 	    	<p class="number">337-989-8796 or 888-467-BABY (2229)</p>
-	    	<form action="index.php" method="post">
-	<div class="search">
-		<input name="searchword" id="mod_search_searchword" maxlength="20" alt="Search" class="inputbox" type="text" size="20" value=""  onblur="if(this.value=='') this.value='search...';" onfocus="if(this.value=='search...') this.value='';" /><input type="submit" value="Search" class="button" onclick="this.form.searchword.focus();"/>	</div>
-	<input type="hidden" name="task"   value="search" />
-	<input type="hidden" name="option" value="com_search" />
-	<input type="hidden" name="Itemid" value="1" />
-</form>
+<div class="search">
+	<?php echo get_search_form(); ?>
+</div><!-- form -->
 	    </td>
 	    <td width="34" align="right" valign="center" class="social">
 	    <table style="width:171px;">
