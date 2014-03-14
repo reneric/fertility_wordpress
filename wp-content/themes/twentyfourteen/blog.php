769a4690aae1cf11814ea8d0cd2d000568f6019f
Ren Simmons <rsimmo4@gmail.com>
2014-03-14 17:30:28 -0500

    youtube icon

--- wp-content/themes/twentyfourteen/blog.php
+++ wp-content/themes/twentyfourteen/blog.php
@@ -14,11 +14,13 @@ get_header(); ?>
 <div id="main-content" class="main-content">
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-			<?php
-				while ( have_posts() ) : the_post();
-					get_template_part( 'content', 'page' );
-				endwhile;
-			?>
+<?php $args = array ( 'post_type' => "post");
+	$custom_query = new WP_Query( $args );
+	if ( $custom_query->have_posts() ):
+	   	while ( $custom_query->have_posts() ) : $custom_query->the_post();
+		get_template_part( 'content', 'page' );
+		 endwhile;
+	endif; ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 	<?php //get_sidebar( 'content' ); ?>
