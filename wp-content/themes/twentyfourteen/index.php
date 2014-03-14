6dda8720362f6cfb59234620c92760f629f34a9a
Ren Simmons <rsimmo4@gmail.com>
2014-03-14 17:25:36 -0500

    youtube icon

--- wp-content/themes/twentyfourteen/index.php
+++ wp-content/themes/twentyfourteen/index.php
@@ -1,61 +1,28 @@
 <?php
 /**
- * The main template file
+ * The template for displaying all pages
  *
- * This is the most generic template file in a WordPress theme and one
- * of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query,
- * e.g., it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
  *
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
-
 get_header(); ?>
-
 <div id="main-content" class="main-content">
-
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
-
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-
-		<?php
-			if ( have_posts() ) :
-				// Start the Loop.
+			<?php
 				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
+					get_template_part( 'content', 'page' );
 				endwhile;
-				// Previous/next post navigation.
-				twentyfourteen_paging_nav();
-
-			else :
-				// If no content, include the "No posts found" template.
-				get_template_part( 'content', 'none' );
-
-			endif;
-		?>
-
+			?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
+	<?php //get_sidebar( 'content' ); ?>
 </div><!-- #main-content -->
-
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
