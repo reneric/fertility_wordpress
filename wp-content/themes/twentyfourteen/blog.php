75b0378a24c2b4cf070c5f07a7e784d32d870561
Ren Simmons <rsimmo4@gmail.com>
2014-03-14 17:27:14 -0500

    youtube icon

---
+++ wp-content/themes/twentyfourteen/blog.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Blog
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+get_header(); ?>
+<div id="main-content" class="main-content">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'page' );
+				endwhile;
+			?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	<?php //get_sidebar( 'content' ); ?>
+</div><!-- #main-content -->
+<?php
+// get_sidebar();
+get_footer();
